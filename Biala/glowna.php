62fc113a91bb861d12189262154fccffab4f08d4
Kolpak24 <jakub.passowicz@gmail.com>
2024-11-10 17:24:21 +0100

    Add files via upload

---
+++ Biala/glowna.php
@@ -0,0 +1,31 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Glowna</title>
+</head>
+
+<style>
+    body {background-color: black;}
+    .biala {text-align: center; color: white; font-size: 36px;}
+    .form {text-align: center; color: white; font-size: 24px;}
+    .menu {text-align: left; color: white ; font-size: 24px;}
+    .lewo{text-align: left;}
+    button {width:100px; height: 25px;}
+</style>
+
+<body>
+    <div class="biala">
+    <h1>Stronnictwo bialej europy</h1>
+    <img src="bialaeuropa.avif" height="311,5" width="512">
+    </div>
+    <div class="biala">
+        W tym miejscu możesz na spokojnie mażyć o białej europie i dizelić się swoimi przemyśleniami z innymi wiernymi tej idei!
+    </div>
+    <br><br><br><br><br><br><br>
+    <div class="menu">
+        <button type="button" onclick="location.href='poszukiwani.php'">POSZUKIWANI</button><br>
+        <button type="button" onclick="location.href=''">SPRZYMIEŻENI</button><br>
+    </div>
+</body>
+</html>
