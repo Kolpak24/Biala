e2ad4f8cdf4ca95572ff066fe99d0fe6ea8e23b4
MEJSON <98404892+MEJSONN@users.noreply.github.com>
2024-11-13 12:15:10 +0100

    Update slogin.php

--- 2biala/slogin.php
+++ 2biala/slogin.php
@@ -1,14 +1,12 @@
 <?php
-$baza = new mysqli('localhost', 'root', '', 'komis24');
-if ($baza->connect_error) {
-    die("Connection failed: " . $baza->connect_error);
-}
-if(@$_POST['sub']){
+$baza = new mysqli('localhost','root', '', 'biala');
+if(isset($_POST['sub'])){
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $sql = "INSERT INTO login (username, email, password)  VALUES ('{$username}', '{$email}', '{$password}')";
+    $sql = "SELECT username, email, password FROM login WHERE username='{$username}' AND email='{$email}' AND '{$password}";
     $baza->query($sql);
     header('location:glowna.php');
     exit();
 }
+?>
