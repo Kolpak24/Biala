09b70e303da26c15c8ef0c2c13422fe2b6dafb42
MEJSON <macie.plaza@gmail.com>
2024-11-19 11:01:41 +0100

    Update glowna.php

--- biala/glowna.php
+++ biala/glowna.php
@@ -13,27 +13,17 @@
         <header class="header1">
             <h1>Stronnictwo białej europy</h1>
             <img src="bialaeuropa.avif" height="311,5" width="512">
-
         </header>
     </section>
-
-    <div class="main">
-    
-        <div class="menu">
-            <button type="button" onclick="location.href='poszukiwani.php'">POSZUKIWANI</button><br>
-            <button type="button" onclick="location.href=''">SPRZYMIEŻENI</button><br>
-        </div>
-
-        <div class="form">
-            W tym miejscu możesz na spokojnie mażyć o białej europie i dizelić się swoimi przemyśleniami z innymi wiernymi tej idei!
-        </div>
-
-        <div class="login">
-            <button type="button" onclick="location.href='login.php'">Zaloguj sie</button><br>
-            <button type="button" onclick="location.href='singup.php'">Zarejestruj sie</button><br>
-        </div>
-
-        
+    <section class="sec2">
+        <button type="button" onclick="location.href='poszukiwani.php'">POSZUKIWANI</button><br>
+        <button type="button" onclick="location.href=''">SPRZYMIEŻENI</button><br>
+        <article>
+            <p>W tym miejscu możesz na spokojnie mażyć o białej europie i dizelić się swoimi przemyśleniami z innymi wiernymi tej idei!</p>
+        </article>
+        <button type="button" onclick="location.href='poszukiwani.php'">POSZUKIWANI</button><br>
+        <button type="button" onclick="location.href=''">SPRZYMIEŻENI</button><br>
+    </section>
     </div>
 </body>
 </html>
