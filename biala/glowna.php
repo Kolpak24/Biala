d92962b459f71a23629af5c31f672deaf0eea2e4
MEJSON <macie.plaza@gmail.com>
2024-11-19 12:54:10 +0100

    Update glowna.php

--- biala/glowna.php
+++ biala/glowna.php
@@ -6,6 +6,40 @@
 </head>
 
 <style>
+    * {
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+    body {
+        background-color: black; 
+        text-align: center;
+    }
+    button {
+        width: 100%;
+        padding: 10px;
+        font-size: 16px;
+    }
+    .sec1 {
+        color: white
+    }
+    .sec2 {
+        display: flex;
+    }
+    .login {
+        flex: 1;
+        color: white;
+        text-align: right;
+
+    }
+    .text1 {
+        flex: 5;
+        color: white;
+        text-align: center;
+        font-size: 22px;
+    }
+    .poszsprzy {
+        text-align: left;
+        flex: 1
+    }
 </style>
 
 <body>
@@ -16,14 +50,17 @@
         </header>
     </section>
     <section class="sec2">
-        <button type="button" onclick="location.href='poszukiwani.php'">POSZUKIWANI</button><br>
-        <button type="button" onclick="location.href=''">SPRZYMIEŻENI</button><br>
-        <article>
+        <section class="login">
+            <button type="button" onclick="location.href='login.php'">Zaloguj sie</button><br>
+            <button type="button" onclick="location.href='singup.php'">Zajerestruj sie</button><br>
+        </section>
+        <section class="text1">
             <p>W tym miejscu możesz na spokojnie mażyć o białej europie i dizelić się swoimi przemyśleniami z innymi wiernymi tej idei!</p>
-        </article>
-        <button type="button" onclick="location.href='poszukiwani.php'">POSZUKIWANI</button><br>
-        <button type="button" onclick="location.href=''">SPRZYMIEŻENI</button><br>
+        </section>
+        <section class="poszsprzy">
+            <button type="button" onclick="location.href='poszukiwani.php'">POSZUKIWANI</button><br>
+            <button type="button" onclick="location.href=''">SPRZYMIEŻENI</button><br>
+        </section>
     </section>
-    </div>
 </body>
 </html>
