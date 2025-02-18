a9c7be85da6900cc0dcd1cb39ee62be08a3f6645
MEJSON <98404892+MEJSONN@users.noreply.github.com>
2025-02-18 16:50:50 +0100

    Add files via upload

--- biala/index.php
+++ biala/index.php
@@ -10,13 +10,8 @@
 
 <body>
     <nav>
-        <ul class="ul_nav">
-            <li class="li_nav">logo.svg</li>
-            <li class="li_nav"></li>
-            <li class="li_nav"></li>
-            <li class="li_nav"></li>
-            <li class="li_nav"></li>
-        </ul>
+        <h3 class="h3_name">Organizacja</h3>
+        <h1 class="h1_name">BIAŁA EUROPA</h1>
     </nav>
     <main>
         <menu class="left_menu">
@@ -27,7 +22,21 @@
                 <li class="li_menu"><a href="#poszukiwani">Poszukiwani</a></li>
             </ul>
         </menu>
-        <article class="art">Artykuł</article>
+        <article class="art">
+            <article class="article">
+                <section>
+                    <h1>Tytuł</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam aut, exercitationem voluptatem
+                        animi officiis, harum eligendi optio in natus distinctio alias qui aperiam recusandae magni
+                        quasi
+                        nostrum molestias? Placeat, et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                        accusamus iste nobis quod inventore. Saepe reiciendis quo, asperiores, porro deleniti distinctio
+                        officiis expedita iure cupiditate repudiandae molestias modi mollitia dolore.</p>
+                </section>
+                <img src="https://imgs.search.brave.com/QnoHaxK5GRyS7SY_j2_0CJfY8vT4C-0aSu47yMz9oNg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/dmVrdG9yZW4tcHJl/bWl1bS9mbGFnZ2Ut/ZGVyLWV1cm9wYWVp/c2NoZW4tdW5pb24t/aW4tZGVyLWZvcm0t/ZGVzLWRlc2lnbnMt/ZXVyb3BhZWlzY2hl/LXVuaW9uLWV1LWZs/YWdnZV8xMDkxMjc5/LTE2NTYuanBnP3Nl/bXQ9YWlzX2h5YnJp/ZA"
+                    alt="zdjecie">
+            </article>
+        </article>
     </main>
     <footer>&copy; 2025 Maciej Płaza & Jakub Passowicz</footer>
 </body>
