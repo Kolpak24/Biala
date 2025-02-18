847fe38111cda4954fe4c80b854a88c5f4c264be
MEJSON <98404892+MEJSONN@users.noreply.github.com>
2025-02-18 10:16:30 +0100

    Add files via upload

--- biala/index.php
+++ biala/index.php
@@ -4,25 +4,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Biala europa</title>
+    <title>Biała Europa</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<nav>Nawigacja</nav>
-<main>
-    <menu class="left_menu">
-        <ul>
-            <li><a href="#art">Artykuly</a></li>
-            <li><a href='#wrogowie'>Wrogowie</a></li>
-            <li><a href='#straceni'>Straceni</a></li>
-            <li><a href='#Poszukiwani'>Poszukiwani</a></li>
-        </ul>
-    </menu>
-    <article class="art">artykuł</article>
-</main>
-<footer>&copy; 2025 Maciej Płaza & Jakub Passowicz</footer>
 
 <body>
-
+    <nav>
+        <ul class="ul_nav">
+            <li class="li_nav">logo.svg</li>
+            <li class="li_nav"></li>
+            <li class="li_nav"></li>
+            <li class="li_nav"></li>
+            <li class="li_nav"></li>
+        </ul>
+    </nav>
+    <main>
+        <menu class="left_menu">
+            <ul class="ul_menu">
+                <li class="li_menu"><a href="#art">Artykuły</a></li>
+                <li class="li_menu"><a href="#wrogowie">Wrogowie</a></li>
+                <li class="li_menu"><a href="#straceni">Straceni</a></li>
+                <li class="li_menu"><a href="#poszukiwani">Poszukiwani</a></li>
+            </ul>
+        </menu>
+        <article class="art">Artykuł</article>
+    </main>
+    <footer>&copy; 2025 Maciej Płaza & Jakub Passowicz</footer>
 </body>
 
 </html>
