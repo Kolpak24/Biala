41b62e3633f2a6da43becead39eb9e84b0f7fa50
MEJSON <98404892+MEJSONN@users.noreply.github.com>
2025-02-18 10:01:17 +0100

    Add files via upload

---
+++ biala/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biala europa</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<nav>Nawigacja</nav>
+<main>
+    <menu class="left_menu">
+        <ul>
+            <li><a href="#art">Artykuly</a></li>
+            <li><a href='#wrogowie'>Wrogowie</a></li>
+            <li><a href='#straceni'>Straceni</a></li>
+            <li><a href='#Poszukiwani'>Poszukiwani</a></li>
+        </ul>
+    </menu>
+    <article class="art">artykuł</article>
+</main>
+<footer>&copy; 2025 Maciej Płaza & Jakub Passowicz</footer>
+
+<body>
+
+</body>
+
+</html>
