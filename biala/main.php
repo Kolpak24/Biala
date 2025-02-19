d1b262584e22fa6765ca1ae975ec3d5a77a26abb
MEJSONN <macie.plaza@gmail.com>
2025-02-19 21:43:16 +0100

    1

---
+++ biala/main.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biala</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav class="body_nav">
+        <div class="nav_logo"><img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/1200px-LEGO_logo.svg.png"
+                art="logo"></div>
+        <div class="nav_name">
+            <h3>Organizacja</h3>
+            <h1>Biala</h1>
+        </div>
+        <div class="nav_profile"></div>
+    </nav>
+    <main>
+        <menu class="left_menu">
+            <ul>
+                <li><a href="#main">Główna</a></li>
+                <li><a href="#articles">Artykuły</a></li>
+                <li><a href="#enemies">Wrogowie</a></li>
+                <li><a href="#lost">Straceni</a></li>
+                <li><a href="#wanted">Poszukiwani</a></li>
+            </ul>
+        </menu>
+        <article class="article">
+            <article class="art_cont">
+                <article class="content">
+                    <h1>Tytul</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ullam sunt fugit ut, laboriosam
+                        consectetur dolores vitae nobis iure ab, mollitia maxime quidem, inventore illum natus ea!
+                        Quidem, atque ipsa.</p>
+                </article>
+            </article>
+        </article>
+    </main>
+    <footer>
+        <p>&copy;2025 Maciej Płaza & Jakub Passowicz</p>
+    </footer>
+</body>
+
+</html>
